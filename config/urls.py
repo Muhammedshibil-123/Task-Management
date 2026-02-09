0b92d557a251bb2bf4f44c9d7d9bcac3fa96ef74
Muhammedshibil-123 <127181540+Muhammedshibil-123@users.noreply.github.com>
2026-02-09 11:26:09 +0530

    latest

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', include('tasks.urls')),
+    path('api/', include('tasks.urls')),
 ]
