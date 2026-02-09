1534701511a218fe5dcb30eea36a024c099b9833
Muhammedshibil-123 <127181540+Muhammedshibil-123@users.noreply.github.com>
2026-02-09 12:37:10 +0530

    dd

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Task
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role Information', {'fields': ('role', 'assigned_admin')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Role Information', {'fields': ('role', 'assigned_admin')}),
+    )
+    list_display = ('email', 'username', 'role', 'is_staff')
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Task)
